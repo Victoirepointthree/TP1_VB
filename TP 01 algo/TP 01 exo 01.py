3f734d01d41354c3f55861af1f004dd0c9ef9e31
Victoirepointthree <54374491+Victoirepointthree@users.noreply.github.com>
2023-10-20 11:34:49 +0200

    Add files via upload

---
+++ TP 01 algo/TP 01 exo 01.py
@@ -0,0 +1,16 @@
+nom = "Système"
+prenom = "Lunaire"
+math = 9
+anglais = 15
+info = 12
+promotion = 2023
+moy= (math + info + anglais)/3
+print(moy)
+print(type(nom))
+print(type(prenom))
+print(type(math))
+print(type(anglais))
+print(type(info))
+print(type(promotion))
+print(type(moy))
+print ("l'etudiant" ,nom ,prenom, "de la promotion ",promotion, " a une moyenne de ",moy,)
---
+++ TP 01 algo/TP 01 exo 02.py
@@ -0,0 +1,5 @@
+jour = 20
+heure = 23
+minute = 59
+calc = (heure * 60 + minute) + jour * 24 * 60
+print(calc)
